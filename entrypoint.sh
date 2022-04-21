79010d22f7edfee5412d2409f40f025805739205
Joel Martin <github@martintribe.org>
2022-04-21 14:44:45 -0500

    LOCAL variable support.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,13 @@ install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -
 # Install build dependencies automatically
 mk-build-deps --install --tool="${install_tool}" debian/control
 
+if [ "${LOCAL}" ]; then
+  echo "Adding +${LOCAL} to package revision"
+  debchange --local=+${LOCAL} "Build for ${LOCAL}"
+  # Remove unwanted trailing 1 from distribution suffix
+  sed -i -e "1s/+${LOCAL}1/+${LOCAL}/" debian/changelog
+fi
+
 # Build the package
 dpkg-buildpackage $@
 
